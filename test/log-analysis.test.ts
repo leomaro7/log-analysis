34d9f6121bdde1f31156033ea43506b9653bbeab
naokis <naokis@gmail.com>
2024-05-29 14:08:26 +0900

    Initial commit

---
+++ test/log-analysis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LogAnalysis from '../lib/log-analysis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/log-analysis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LogAnalysis.LogAnalysisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

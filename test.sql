e7981a313621fd7ffbb73338ca434b01293ac24b
naokis <naokis@gmail.com>
2024-06-06 21:23:19 +0900

    10 commit

--- test.sql
+++ test.sql
@@ -2,6 +2,33 @@ SELECT date_format(
 		date_add(
 			'hour',
 			9,
+			date_parse(substr(time, 1, 13), '%Y-%m-%dT%H')
+		),
+		'%Y-%m-%d %H:00:00'
+	) AS log_hour_jst,
+	elb,
+	COUNT(*) AS request_count,
+	SUM(received_bytes) AS total_received_bytes,
+	SUM(sent_bytes) AS total_sent_bytes
+FROM data_catalog_alb.alb_table
+WHERE parse_datetime(time, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z') BETWEEN timestamp '2024-06-06 00:00 Asia/Tokyo'
+	AND timestamp '2024-06-06 12:00 Asia/Tokyo'
+	AND date BETWEEN '2024/06/03' AND '2024/06/06'
+	AND accountid = '268546037544'
+GROUP BY date_add(
+		'hour',
+		9,
+		date_parse(substr(time, 1, 13), '%Y-%m-%dT%H')
+	),
+	elb
+ORDER BY log_hour_jst,
+	elb;
+
+
+SELECT date_format(
+		date_add(
+			'hour',
+			9,
 			date_parse(substr(time, 1, 10), '%Y-%m-%d')
 		),
 		'%Y-%m-%d'
